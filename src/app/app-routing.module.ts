6a9e281200066a7b01ff99322452fea997ecde12
SUPERTRONICA <eduxworks@gmail.com>
2021-04-15 01:46:53 -0500

    Home Page, es la pagina principal, se crea una Card con una imagen, se agragan 4 botones

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,32 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { HomePage } from './home/home.page';
 
 const routes: Routes = [
+  {
+    path: 'home',
+    component: HomePage
+
+  },
   {
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    path: 'cathombres',
+    loadChildren: () => import('./cathombres/cathombres.module').then( m => m.CathombresPageModule)
+  },
+  {
+    path: 'catmujeres',
+    loadChildren: () => import('./catmujeres/catmujeres.module').then( m => m.CatmujeresPageModule)
+  },
+  {
+    path: 'catninos',
+    loadChildren: () => import('./catninos/catninos.module').then( m => m.CatninosPageModule)
+  },
+  {
+    path: 'catninas',
+    loadChildren: () => import('./catninas/catninas.module').then( m => m.CatninasPageModule)
   },
 ];
 
